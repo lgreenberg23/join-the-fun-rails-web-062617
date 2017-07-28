1050c3c3269afe75de7d0230ae58e079a2be55ab
github username <github email address>
2017-07-28 11:43:43 -0400

    Done.

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,2 +1,12 @@
 class Passenger < ActiveRecord::Base
+	has_many :rides
+	has_many :taxis, through: :rides
 end
+
+
+    # create_table :appointments do |t|
+    #   t.belongs_to :physician, index: true
+    #   t.belongs_to :patient, index: true
+    #   t.datetime :appointment_date
+    #   t.timestamps
+    # end
